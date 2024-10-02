8699c9f0f0618c816319053a46ad461f069fdd8d
Matte Vonnee <mattevonnee@gmail.com>
2024-10-02 18:53:55 -0400

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,14 @@
 import styles from "./page.module.css";
-import { Helmet } from "react-helmet";
 
 export default function Home() {
   return (
-
-    <div>
-      <Helmet>
-        <title>MATTE VONNÉE</title>
-      </Helmet>
-
-      <div className={styles.page}>
-        <main className={styles.main}>
-          <text>matte vonnee</text>
-        </main>
-        <footer className={styles.footer}>
-          <text>copyright</text>
-        </footer>
-      </div>
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <text>matte vonnee</text>
+      </main>
+      <footer className={styles.footer}>
+        <text>copyright</text>
+      </footer>
     </div>
   );
 }
