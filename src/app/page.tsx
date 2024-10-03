e8ad32cfc34f4ea748f51cac9d9ad072deccc68a
Matte Vonnee <mattevonnee@gmail.com>
2024-10-03 08:41:26 -0400

    new changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
-import styles from "./page.module.css";
 
-export default function Home() {
+const HomePage = () => { 
+  
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <text>matte vonnee</text>
-      </main>
-      <footer className={styles.footer}>
-        <text>copyright</text>
-      </footer>
-    </div>
-  );
+    <div> <text>Matte Vonnée
+    </text>
+  </div>
+  )
 }
+
+export default HomePage
